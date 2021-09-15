7eda4990f60717f2dedc0b3c9eba612d33c79602
Allen <allenshaw19@gmail.com>
2021-09-16 00:53:39 +0800

    更新gitignore删除bin文件

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-PROJ=godis
+mkdir -p output/server output/client
 
-mkdir -p output/bin
-go build --race -v -o $PROJ
-mv $PROJ output/bin
+echo "build godis server"
+cd server
+./build.sh
+cp -r output output/server
+
+echo "build godis client"
+cd client
+./build.sh
+cp -r output output/client
